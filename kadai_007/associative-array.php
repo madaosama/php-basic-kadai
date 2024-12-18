f13166f3c38139980068b1697406d62082023f80
madaosama <64106920+madaosama@users.noreply.github.com>
2024-12-18 20:32:25 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $associative_array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($associative_array);
+        ?>
+    </p>
+</body>
+
+</html>
